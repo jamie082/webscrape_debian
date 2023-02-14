0ea4b2ee678d9f0f6fa31d7ba53fa64b4de861da
jamie082 <jamiemorrissey02@gmail.com>
2023-02-14 01:40:05 -0800

    added files

--- scrape_portfolio.py
+++ scrape_portfolio.py
@@ -2,6 +2,7 @@
 
 from bs4 import BeautifulSoup
 import requests
+import re
 
 website = requests.get('http://debian.org/doc/manuals/debian-handbook/')
 soup = BeautifulSoup(website.content, 'html.parser')
@@ -12,17 +13,20 @@ links_find_search = links_find.find_all('a')
 
 for soup in links_find_search:
     names = links_find.contents[0]
-    print(soup.string)
+    print (soup.string)
 
 #  second additional code to extract link to either pandas or export to XML document
+
+# find all the anchor tags with "href" 
+# attribute strting with "http"
 
 images_list = []
 
 images = soup.select('a')
-for xml_document in images:
-    src = image.get('src')
-    alt = image.get('alt')
-    images_list.append({"src": src, "alt": alt})
+for image in images:
+    src = image.get('href')
+
+    images_list.append({"src": src})
 
 for image in images_list:
     print(image)
